e6740173dd345c4cabe73b14fa8254bb655a6351
hoangdung59 <dungthuc11@gmail.com>
2025-03-26 13:53:55 +0700

    them model, interface, designPantter

---
+++ app/src/main/java/dunghtph30405/example/nhom1_pro1121/inteface/TotalUpdateListener.java
@@ -0,0 +1,5 @@
+package dunghtph30405.example.nhom1_pro1121.inteface;
+
+public interface TotalUpdateListener {
+    void onTotalUpdated(double total);
+}
---
+++ app/src/main/java/dunghtph30405/example/nhom1_pro1121/model/photo.java
@@ -0,0 +1,18 @@
+package dunghtph30405.example.nhom1_pro1121.model;
+
+
+public class photo {
+    private int photoID;
+
+    public photo(int photoID) {
+        this.photoID = photoID;
+    }
+
+    public int getPhotoID() {
+        return photoID;
+    }
+
+    public void setPhotoID(int photoID) {
+        this.photoID = photoID;
+    }
+}
